2fe22863c997393659f2e115c5382bb2ac2f2963
Illia559 <123123ilja@gmail.com>
2023-10-10 20:31:47 +0300

    added second task of the first lection

---
+++ Lection 1/Task 2/main.py
@@ -0,0 +1,3 @@
+userNumber1 = int(input("Enter first number: "))
+userNumber2 = int(input("Enter second number: "))
+print("{} + {} = {}" .format(userNumber1, userNumber2, userNumber1 + userNumber2))
